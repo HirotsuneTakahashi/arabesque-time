0e992c6a859f6b4cf921eeb3fd81ed5303efdb41
Hirotsune-711 <162408750+Hirotsune-711@users.noreply.github.com>
2025-07-12 14:38:05 +0900

    アプリケーションのポートを5000から5001に変更し、requirements.txtにgunicornを追加しました。

--- app.py
+++ app.py
@@ -297,4 +297,4 @@ if __name__ == '__main__':
         db.create_all()
     
     # Flaskアプリケーションの起動
-    app.run(debug=True, host='0.0.0.0', port=5000) 
+    app.run(debug=True, host='0.0.0.0', port=5001) 
